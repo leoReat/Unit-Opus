3036abc3ec865783b3fc782ac3126b94c30771d8
leoReat <l.estlimbaum@gmail.com>
2017-06-15 18:07:28 +0200

    mise en place google analytics

--- index.php
+++ index.php
@@ -19,6 +19,9 @@
 
 
 	<script src="js/jquery.min.js"></script>
+
+    <!-- SCRIPT HOTJAR -->
+
 	<script>
 	    (function(h,o,t,j,a,r){
 	        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
@@ -30,10 +33,24 @@
 	    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
 	</script>
 
+    <!-- SCRIPT ANALYTICS -->
+
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-101090405-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
+
 	<link href="https://fonts.googleapis.com/css?family=Assistant:300,400,600,700,800" rel="stylesheet">
 
 </head>
 <body>
+<?php include_once("analyticstracking.php") ?>
 	<header>
 		<nav>
 			<a href="#presentation" class="home no-mobile"><img src="images/logo.png" alt="Logo Opus" id="logo" /></a>

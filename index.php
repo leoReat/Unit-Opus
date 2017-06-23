82ec55eeb3bb5a3a84dc449a2dabda32ead95241
leoReat <l.estlimbaum@gmail.com>
2017-06-23 17:26:24 +0200

    W3C validator

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
 <body>
 	<header>
 		<nav>
-			<a href="#presentation" class="home no-mobile"><img src="<?php echo $urlBASE; ?>images/logo.png" alt="Logo Opus" id="logo" /></a>
+			<a href="#presentation" class="home no-mobile"><img src="<?php echo $urlBASE; ?>images/logo.png" alt="Logo Opus" id="logo" title="opus logotype"/></a>
 			<ul class="navigation">
 				<li class="active"><a href="#presentation"><?php echo ACCUEIL; ?></a></li>
 				<li><a href="#concept"><?php echo CONCEPT; ?></a></li>
@@ -63,8 +63,8 @@
 
 					$urlPro = (isset($_GET["pro"])) ? "/pro" : "";
 				?>
-				<li class="<?php echo $noMobileFr; ?>"><a href="<?php echo $urlPro; ?>/fr"><img src="<?php echo $urlBASE; ?>images/francais.png" alt="français"/></a></li>
-				<li class="<?php echo $noMobileEn; ?>"><a href="<?php echo $urlPro; ?>/en"><img src="<?php echo $urlBASE; ?>images/english.png" alt="english"/></a></li>
+				<li class="<?php echo $noMobileFr; ?>"><a href="<?php echo $urlPro; ?>/fr"><img src="<?php echo $urlBASE; ?>images/francais.png" alt="français" title="redirection page français"/></a></li>
+				<li class="<?php echo $noMobileEn; ?>"><a href="<?php echo $urlPro; ?>/en"><img src="<?php echo $urlBASE; ?>images/english.png" alt="english" title="redirection page anglais"/></a></li>
 			</ul>
 		</nav>
 	</header>
@@ -77,7 +77,7 @@
 
 		<article id="concept">
 			<div class="content">
-				<img src="<?php echo $urlBASE; ?>images/iphone.png" class="center" alt=""/>
+				<img src="<?php echo $urlBASE; ?>images/iphone.png" class="center" alt="mockup opus" title="visuelle télephone opus"/>
 				<div class="center">
 				<?php
 					require_once("assets/concept".$pro.".html")
@@ -88,7 +88,7 @@
 					<div id="connect">
 						<a href="#concept" class="button inscription"><?php echo CONCEPT_CTA ?></a> <br>
 						<form>
-							<input type="text" name="" placeholder="Adresse mail">
+							<input type="text" name="mailing" placeholder="Adresse mail">
 							<button>OK</button>
 							<p>OU</p>
 							<a href="#concept" class="button google connect">Google</a>
@@ -106,18 +106,18 @@
 					<h1><?php echo CONTACT_TITRE; ?></h1>
 					<p><?php echo CONTACT_TEXTE; ?></p>
 					<ul>
-						<li><img src="<?php echo $urlBASE; ?>images/tel.png" alt="logo telephone" /> 06.70.28.61.80</li>
-						<li><img src="<?php echo $urlBASE; ?>images/map.png" alt="logo telephone" /> OPUS<br>
+						<li><img src="<?php echo $urlBASE; ?>images/tel.png" alt="logo telephone" title="pictogramme téléphone"/> 06.70.28.61.80</li>
+						<li><img src="<?php echo $urlBASE; ?>images/map.png" alt="logo telephone" title="pictogramme carte"/> OPUS<br>
 						<span>5 rue Froment, 75011 Paris - FRANCE</span></li>
-						<li><img src="<?php echo $urlBASE; ?>images/mail.png" alt="logo telephone" /> <a href="mailto:unit.iesa@gmail.com">unit.iesa@gmail.com</a></li>
+						<li><img src="<?php echo $urlBASE; ?>images/mail.png" alt="logo telephone" title="pictogramme mail"/> <a href="mailto:unit.iesa@gmail.com">unit.iesa@gmail.com</a></li>
 					</ul>
 					<ul class="social">
-						<li><a href="https://www.facebook.com/GetOpusApp" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/fb.png" /></a></li>
-						<li><a href="https://twitter.com/GetOpus" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/tw.png" /></a></li>
-						<li><a href="https://www.instagram.com/getopusapp/" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/insta.png" /></a></li>
+						<li><a href="https://www.facebook.com/GetOpusApp" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/fb.png" alt="lien facebook opus" title="redirection facebook"/></a></li>
+						<li><a href="https://twitter.com/GetOpus" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/tw.png" alt="lien twitter opus" title="redirection twitter"/></a></li>
+						<li><a href="https://www.instagram.com/getopusapp/" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/insta.png" alt="lien instagram opus" title="redirection instagram"/></a></li>
 						<!-- <li><a href="" target="_BLANK"><img src="images/yt.png" /></a></li> -->
 						<!-- <li><a href="https://www.linkedin.com/in/opus-app-67347b145/" target="_BLANK"><img src="images/lkd.png" /></a></li> -->
-						<li><a href="http://m.me/GetOpusApp" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/messenger.svg" alt="live chat messenger avec Opus" /></a></li>
+						<li><a href="http://m.me/GetOpusApp" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/messenger.svg" alt="live chat messenger avec Opus" title="discussion avec opus"/></a></li>
 					</ul>
 				</div>
 				
@@ -138,7 +138,7 @@
 		<p><?php echo MENTIONS_TEXTE; ?></p>
 	</div>
 
-	<button class="scroll-top" href="#presentation"><img src="<?php echo $urlBASE; ?>images/scroll-top.png" alt="Retour en haut de page" /></button>
+	<button class="scroll-top" href="#presentation"><img src="<?php echo $urlBASE; ?>images/scroll-top.png" alt="Retour en haut de page" title="aller vers le haut"/></button>
 
 		
     <!-- COOKIES -->

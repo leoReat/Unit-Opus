f7b1a49a95219ecef0861b9479d845eb06b93351
Thomas Deroua <thomasderoua@hotmail.com>
2017-06-26 09:36:13 +0200

    add config.php

--- assets/config.php
+++ assets/config.php
@@ -3,8 +3,4 @@
 	$pro = (isset($_GET["pro"])) ? "_pro" : "";
 	require_once("lang/".$lang.".lang.php");
 
-<<<<<<< HEAD
 	$urlBASE = "/Unit-opus/";
-=======
-	$urlBASE = "";
->>>>>>> 718714b09b5ab0d3bed8fb027f3a266b3199e28f

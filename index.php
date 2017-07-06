4b83762775ae80bbe028801ebb54a80afc0a20ac
Thomas Deroua <thomasderoua@hotmail.com>
2017-07-06 23:14:44 +0200

    first strate landing v7

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
 <?php
-	require_once("assets/config.php");
+require_once("assets/config.php");
 ?>
 <!doctype html>
 <head>
-	<title>OPUS - Digitalisez votre musée</title>
-	<link rel="stylesheet" type="text/css" href="<?php echo $urlBASE; ?>css/museum.css">
-	<link rel="icon" type="image/png" href="<?php echo $urlBASE; ?>images/favicon.png" />
+    <title>OPUS - Digitalisez votre musée</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo $urlBASE; ?>css/museum.css">
+    <link rel="icon" type="image/png" href="<?php echo $urlBASE; ?>images/favicon.png"/>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="keywords" content="Opus, musée, digital">
-	<meta name="description" content="Opus est une solution innovante de digitalisation des musées et galeries." />
-	<meta name="title" content="Opus - Digitalisez votre musée">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="keywords" content="Opus, musée, digital">
+    <meta name="description" content="Opus est une solution innovante de digitalisation des musées et galeries."/>
+    <meta name="title" content="Opus - Digitalisez votre musée">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
 
-	<script src="<?php echo $urlBASE; ?>js/jquery.min.js"></script>
+    <script src="<?php echo $urlBASE; ?>js/jquery.min.js"></script>
 
     <!-- SCRIPT HOTJAR -->
 
-	<script>
-	    // (function(h,o,t,j,a,r){
-	    //     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-	    //     h._hjSettings={hjid:530706,hjsv:5};
-	    //     a=o.getElementsByTagName('head')[0];
-	    //     r=o.createElement('script');r.async=1;
-	    //     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-	    //     a.appendChild(r);
-	    // })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-	</script>
+    <script>
+        // (function(h,o,t,j,a,r){
+        //     h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        //     h._hjSettings={hjid:530706,hjsv:5};
+        //     a=o.getElementsByTagName('head')[0];
+        //     r=o.createElement('script');r.async=1;
+        //     r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        //     a.appendChild(r);
+        // })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
 
     <!-- SCRIPT ANALYTICS -->
 
@@ -36,126 +36,126 @@
         //         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         //     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         // })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-		//
+        //
         // ga('create', 'UA-101090405-1', 'auto');
         // ga('send', 'pageview');
 
     </script>
 
-	<link href="https://fonts.googleapis.com/css?family=Assistant:300,400,600,700,800" rel="stylesheet">
-
 </head>
 <body class="museum">
-	<header>
-		<nav>
-			<h1>
-				<a href="#" class="home no-mobile">
-				<img src="<?php echo $urlBASE; ?>images/logo-blanc.png" alt="Logo Opus" id="logo" title="Logo de l'application Opus" />
-				<img src="<?php echo $urlBASE; ?>images/logo.png" alt="Logo Opus" id="logo" title="Logo de l'application Opus" class="logo2" />
-				</a>
-			</h1>
+<header>
+    <nav>
+        <h1>
+            <a href="#" class="home no-mobile">
+                <img src="<?php echo $urlBASE; ?>images/logo-blanc.png" alt="Logo Opus" id="logo"
+                     title="Logo de l'application Opus"/>
+                <img src="<?php echo $urlBASE; ?>images/logo.png" alt="Logo Opus" id="logo"
+                     title="Logo de l'application Opus" class="logo2"/>
+            </a>
+        </h1>
 
-			<form method="POST">
-				<select class="lang" name="lang">
-					<option value="fr"<?php echo ($lang == "fr") ? " selected" : ""; ?>>Français</option>
-					<option value="en"<?php echo ($lang == "en") ? " selected" : ""; ?>>Anglais</option>
-					<option value="it"<?php echo ($lang == "it") ? " selected" : ""; ?>>Italien</option>
-				</select>
-			</form>
-		</nav>
-	</header>
-	<section>
-
-		<article id="concept" class="concept">
-			<!-- <div id="bg1"></div>
-			<div id="bg2"></div>
-			<div id="bg3"></div> -->
-
-			<div class="content">
-				<img src="<?php echo $urlBASE; ?>images/joconde.png" alt="mockup opus" title="visuelle télephone opus" class="iPhone"/>
-
-				<div class="center strate1">
-					<h2>Opus</h2>
-					<h2>La boutique musée embarquée</h2>
-				</div>
-
-				<div class="bottom">
-					<h3>Commencez la visite</h3>
-					<div class="arrow"></div>
-				</div>
-			</div>
-		</article>
-
-		<article class="concept">
-			<div class="center strate2">
-				<h2>La joconde au musée</h2>
-				<button type="button" name="button"><img src="<?php echo $urlBASE; ?>images/more.png" alt="lire plus" title="Obtenir plus d'informations"/></button>
-				<p>L'oeuvre vous plaît ? Aucun problème. Scannez l'oeuvre grâce à votre application Opus et ajoutez là à vos favoris. Vous aurez accès accès à des informations, dans la langue que vous souhaitez, sur le tableau ainsi qu'une boutique embarquée réservée à celui-ci.</p>
-			</div>
-		</article>
-
-		<article class="concept">
-			<div class="center strate3">
-				<h2>La joconde en boutique</h2>
-				<button type="button" name="button"><img src="<?php echo $urlBASE; ?>images/more.png" alt="lire plus" title="Obtenir plus d'informations"/></button>
-				<p>Opus vous propose une sélection de représentations et de produits, que vous pouvez directement payer via votre smartphone, depuis le musée ou même sur la terrasse d'un café, après la visite.</p>
-			</div>
-		</article>
-
-		<article class="concept">
-			<div class="center strate4">
-				<h2>La joconde chez vous</h2>
-				<button type="button" name="button"><img src="<?php echo $urlBASE; ?>images/more.png" alt="lire plus" title="Obtenir plus d'informations"/></button>
-				<p>Le système de livraison vous permet de ne pas vous encombrer. Recevez votre reproduction directement chez vous et contemplez la à votre guise.</p>
-			</div>
-		</article>
-	</section>
-	<footer>
-		<div id="contact">
-			<h2>Vivez l'expérience,<br> rejoignez-nous sur les réseaux sociaux.</h2>
-			<ul class="social">
-				<li><a href="https://www.facebook.com/GetOpusApp" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/fb.png" alt="lien facebook opus" title="redirection facebook"/></a></li>
-				<li><a href="https://twitter.com/GetOpus" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/tw.png" alt="lien twitter opus" title="redirection twitter"/></a></li>
-				<li><a href="https://www.instagram.com/getopusapp/" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/insta.png" alt="lien instagram opus" title="redirection instagram"/></a></li>
-				<!-- <li><a href="" target="_BLANK"><img src="images/yt.png" /></a></li> -->
-				<!-- <li><a href="https://www.linkedin.com/in/opus-app-67347b145/" target="_BLANK"><img src="images/lkd.png" /></a></li> -->
-				<li><a href="http://m.me/GetOpusApp" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/messenger.png" alt="live chat messenger avec Opus" title="discussion opus"/></a></li>
-			</ul>
-
-			<h3>Vous êtes professionnel ?</h3>
-			<a href="/pro" class="button">En savoir plus</a>
-
-		</div>
-		<p>Opus - <?php echo COPYRIGHT; ?> © 2017</p>
-		<p><a href=""><?php echo MENTIONS; ?></a></p>
-	</footer>
+        <form method="POST">
+            <select class="lang" name="lang">
+                <option value="fr"<?php echo ($lang == "fr") ? " selected" : ""; ?>>Français</option>
+                <option value="en"<?php echo ($lang == "en") ? " selected" : ""; ?>>Anglais</option>
+                <option value="it"<?php echo ($lang == "it") ? " selected" : ""; ?>>Italien</option>
+            </select>
+        </form>
+    </nav>
+</header>
 
 
+<main id="panel-container">
 
-	<div class="overlay"></div>
-	<div class="modal">
-		<button class="close">x</button>
-		<h2><?php echo MENTIONS_TITRE; ?></h2>
-		<p><?php echo MENTIONS_TEXTE; ?></p>
-	</div>
+    <article class="concept" id="panel1">
+       <div class="center">
+           <h2>Opus<br>La boutique musée embarquée</h2>
+           <h3>Procurez-vous des  souvenirs, tout en préservant les œuvres</h3>
+       </div>
+    </article>
 
-    <!-- COOKIES -->
-    <div id="cookies"></div>
-    <script src="<?php echo $urlBASE; ?>js/cookiechoices.js"></script>
-    <script>document.addEventListener('DOMContentLoaded',
-            function(event){
-        cookieChoices.showCookieConsentBar('<?php echo COOKIES; ?>', '<?php echo COOKIES_AGREE; ?>', '<?php echo COOKIES_LEARN; ?>', "http://get-opus.fr", '<?php echo COOKIES2; ?>');
+    <article class="concept" id="panel2">
+        <h2>La joconde au musée</h2>
+        <p>L'oeuvre vous plaît ? Aucun problème. Scannez l'oeuvre grâce à votre application Opus et ajoutez là à vos
+            favoris. Vous aurez accès accès à des informations, dans la langue que vous souhaitez, sur le tableau ainsi
+            qu'une boutique embarquée réservée à celui-ci.</p>
+    </article>
 
-            $("footer p:nth-of-type(2) a, .overlay, button.close, #cookieChoiceInfo a:nth-of-type(1)").click(function(e){
+    <article class="concept" id="panel3">
+        <h2>La joconde en boutique</h2>
+        <p>Opus vous propose une sélection de représentations et de produits, que vous pouvez directement payer via
+            votre smartphone, depuis le musée ou même sur la terrasse d'un café, après la visite.</p>
+    </article>
+
+    <article class="concept" id="panel4">
+        <h2>La joconde chez vous</h2>
+        <p>Le système de livraison vous permet de ne pas vous encombrer. Recevez votre reproduction directement chez
+            vous et contemplez la à votre guise.</p>
+    </article>
+
+    <div class="bottom">
+        <h3><a href='#panel1'>•</a> <a href='#panel2'>•</a> <a href='#panel3'>•</a> <a href='#panel4'>•</a></h3>
+        <div class="arrow"></div>
+    </div>
+
+    <!--<div class="" id="artwork"></div>
+    <div class="" id="artwork-detail"></div> -->
+</main>
+
+<footer style=display:none;">
+    <div id="contact">
+        <h2>Vivez l'expérience,<br> rejoignez-nous sur les réseaux sociaux.</h2>
+        <ul class="social">
+            <li><a href="https://www.facebook.com/GetOpusApp" target="_BLANK"><img
+                            src="<?php echo $urlBASE; ?>images/fb.png" alt="lien facebook opus"
+                            title="redirection facebook"/></a></li>
+            <li><a href="https://twitter.com/GetOpus" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/tw.png"
+                                                                           alt="lien twitter opus"
+                                                                           title="redirection twitter"/></a></li>
+            <li><a href="https://www.instagram.com/getopusapp/" target="_BLANK"><img
+                            src="<?php echo $urlBASE; ?>images/insta.png" alt="lien instagram opus"
+                            title="redirection instagram"/></a></li>
+            <!-- <li><a href="" target="_BLANK"><img src="images/yt.png" /></a></li> -->
+            <!-- <li><a href="https://www.linkedin.com/in/opus-app-67347b145/" target="_BLANK"><img src="images/lkd.png" /></a></li> -->
+            <li><a href="http://m.me/GetOpusApp" target="_BLANK"><img src="<?php echo $urlBASE; ?>images/messenger.png"
+                                                                      alt="live chat messenger avec Opus"
+                                                                      title="discussion opus"/></a></li>
+        </ul>
+
+        <h3>Vous êtes professionnel ?</h3>
+        <a href="/pro" class="button">En savoir plus</a>
+
+    </div>
+    <p>Opus - <?php echo COPYRIGHT; ?> © 2017</p>
+    <p><a href=""><?php echo MENTIONS; ?></a></p>
+</footer>
+
+
+<div class="overlay"></div>
+<div class="modal">
+    <button class="close">x</button>
+    <h2><?php echo MENTIONS_TITRE; ?></h2>
+    <p><?php echo MENTIONS_TEXTE; ?></p>
+</div>
+
+<!-- COOKIES -->
+<div id="cookies"></div>
+<script src="<?php echo $urlBASE; ?>js/cookiechoices.js"></script>
+<script>document.addEventListener('DOMContentLoaded',
+        function (event) {
+            cookieChoices.showCookieConsentBar('<?php echo COOKIES; ?>', '<?php echo COOKIES_AGREE; ?>', '<?php echo COOKIES_LEARN; ?>', "http://get-opus.fr", '<?php echo COOKIES2; ?>');
+
+            $("footer p:nth-of-type(2) a, .overlay, button.close, #cookieChoiceInfo a:nth-of-type(1)").click(function (e) {
                 e.preventDefault();
                 $(".modal, .overlay").fadeToggle(300);
                 $("body").toggleClass("no-scroll");
             });
-    });
-    </script>
+        });
+</script>
 
-	<script src="https://www.gstatic.com/firebasejs/4.1.2/firebase.js"></script>
-	<script src="<?php echo $urlBASE; ?>js/museum.js"></script>
+<!-- <script src="http://www.gstatic.com/firebasejs/4.1.2/firebase.js"></script> -->
+<script src="<?php echo $urlBASE; ?>js/museum.js"></script>
 
 </body>
 </html>
--- js/museum.js
+++ js/museum.js
@@ -1,95 +1,81 @@
-$(document).ready(function(){
-    $(document).scroll(function(){
-        if($(window).width() < 800) return;
-        var topScroll = $(window).scrollTop();
-        var heightWindow = $(window).height();
-        var nbStrate = topScroll / heightWindow;
+(function ($) {
 
-        var strate1 = nbStrate / 0.5;
+    $('#panel1').addClass('focus');
+    $(window).scrollTop(0);
 
-        if(strate1 < 1){
-            var height = 300 + (100 * strate1);
-            var transform = 50 * strate1;
-            var padding = 150 - (80 * strate1);
-            $(".concept img.iPhone").css({"height":"calc(100% - "+height+"px)", "transform":"translate("+transform+"%, -50%)", "right":transform+"%"});
-            $(".strate1").show().css("opacity", 1-strate1);
-            $("#bg1").css("opacity", 1-strate1);
-            $(".concept div.center.strate2").show().css("opacity", strate1);
-            $("#logo").show().css("opacity", 1-strate1);
-            $(".logo2").show().css("opacity", strate1);
+    var scrollTo = function(id){
+        $('.concept.focus').removeClass('focus');
+        $('.concept#panel'+id).addClass('focus');
 
+        var offset = $('.concept#panel'+panel).offset().top;
+        $(window).scrollTop(offset);
+        prevScroll = offset;
+
+        $(".bottom a").css("opacity", "0.5");
+        $(".bottom a:nth-of-type("+id+")").css("opacity", "1");
+        panel = id;
+    };
+
+
+    var panel = 1, prevScroll = 0, stop = 0;
+
+    $("body").on("scroll mousewheel touchmove", function(e) {
+        //if ($(window).width() < 800) return;
+
+
+        if(stop === 1) {
+            //e.preventDefault();
+            return;
+        }
+
+        var st = $(this).scrollTop();
+
+        console.log(prevScroll, st)
+        if(st >= prevScroll){
+            console.log("next");
+            nextSlide();
         }
         else{
-            $(".concept img.iPhone").css({"height":"calc(100% - 400px)", "transform":"translate(50%, -85%)", "right":"50%"})
-            $(".strate1").fadeOut();
-            $("#bg1").css("opacity", 0);
-            $("#logo").show().css("opacity", 0);
-            $(".logo2").show().css("opacity", 1);
-
-            if(nbStrate > 2.5){
-                $("#bg3").css("opacity", 1);
-                $(".concept div.center.strate3").fadeOut();
-                $(".concept div.center.strate4").fadeIn();
-            }
-            else if(nbStrate > 2){
-                $("#bg3").css("opacity", (nbStrate-2) * 2);
-                $(".concept div.center.strate3").show().css("opacity", 1-(nbStrate-2) * 2);
-                $(".concept div.center.strate4").show().css("opacity", (nbStrate-2) * 2);
-            }
-            else{
-                $("#bg3").css("opacity", 0);
-                $(".concept div.center.strate4").fadeOut();
-
-
-                if(nbStrate > 1.5){
-                    $("#bg2").css("opacity", 1);
-                    $(".concept div.center.strate2").fadeOut();
-                    $(".concept div.center.strate3").fadeIn();
-                }
-                else if(nbStrate > 1){
-                    $("#bg2").css("opacity", (nbStrate-1)*2);
-                    $(".concept div.center.strate2").show().css("opacity", 1-(nbStrate-1)*2);
-                    $(".concept div.center.strate3").show().css("opacity", (nbStrate-1)*2);
-                }
-                else{
-                    $("#bg2").css("opacity", 0);
-                    $(".concept div.center.strate2").fadeIn();
-                    $(".concept div.center.strate3").fadeOut();
-                }
-            }
-
-            if(nbStrate > 3) $(".bottom .arrow").css("transform", "rotate(90deg) translateY(50%) scaleX(-1)");
-            else $(".bottom .arrow").css("transform", "rotate(90deg) translateY(50%)");
-
-            $(".bottom h3").css("opacity", 0);
+            console.log("PREV")
+            prevSlide();
         }
-
-        var index = Math.ceil(topScroll / heightWindow) + 1;
-        $(".strate2, .strate4").css("margin-top", -(topScroll / 70 )+"px")
-        $(".strate1, .strate3").css("margin-top", (topScroll / 70 )+"px")
     });
 
-    // $(".concept div.center button").click(function(e){
-    //     // var topScroll = $(window).scrollTop();
-    //     // var heightWindow = $(window).height();
-    //     // var nbStrate = Math.ceil(topScroll / heightWindow);
-    //     // if(nbStrate > 4) nbStrate = 0;
-    //     //
-    //     // // $(".center p").fadeOut();
-    //     // $(".strate"+(nbStrate)+" p").fadeToggle();
-    //
-    //     // $('html, body').animate( { scrollTop: heightWindow * nbStrate }, 500 ); // Go
-    //
-    //     var buttonClass = $(this).closest(".center").attr("class").replace("center ", "");
-    //     $("."+buttonClass+" p").fadeToggle();
-    // });
+    $(".bottom a:nth-of-type("+panel+")").css("opacity", "1");
 
-    $(".bottom").click(function(){
-        var topScroll = $(window).scrollTop();
-        var heightWindow = $(window).height();
-        var nbStrate = Math.ceil(topScroll / heightWindow) + 1;
-        if(nbStrate > 4) nbStrate = 0;
 
-        $('html, body').animate( { scrollTop: heightWindow * nbStrate }, 1000 ); // Go
+
+    $(".bottom a").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+
+        scrollTo($(this).index() + 1);
     });
-});
+
+    $(".bottom .arrow").click(function(){
+        nextSlide();
+    });
+
+    var nextSlide = function(){
+        panel ++;
+        if(panel > 4) panel = 1;
+        scrollTo(panel);
+
+        stop = 1;
+        setTimeout(function(){
+            stop = 0;
+        }, 1000);
+    }
+
+    var prevSlide = function(){
+        if(panel > 1) panel --;
+        console.log(panel);
+        scrollTo(panel);
+
+        stop = 1;
+        setTimeout(function(){
+            stop = 0;
+        }, 1000);
+    }
+
+})(jQuery)
